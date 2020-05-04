bf35afb7ae3de4bf25eadeaf89d0aeec7821806e
BeeTwin <mr.hunterdan@mail.ru>
2020-05-04 17:08:47 +0500

    CheckLine(without sql)

--- SemesterWork/Encoder866.cs
+++ SemesterWork/Encoder866.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 
 namespace SemesterWork
 {
-    public static class Encoder866
+    public static class Encoder866 // TODO: переименовать
     {
         public static Encoding Encoding = Encoding.GetEncoding(866);
 
