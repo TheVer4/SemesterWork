4e7a6a171c9a6420e2f8daf621bb2bff3f99f8b9
Nikita <Nikita@TheVer4.ru>
2020-05-07 20:45:32 +0500

    Fixed barcode addition with amount

--- SemesterWork/EventHandler.cs
+++ SemesterWork/EventHandler.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.ComponentModel;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -13,7 +11,7 @@ namespace SemesterWork
     public partial class MainWindow
     {
 
-        private bool _updatedSomeValue;
+        private string _readBarcode;
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
@@ -26,27 +24,38 @@ namespace SemesterWork
             
         }
 
-        private void BarcodeReaded(object sender, SerialDataReceivedEventArgs args)
+        private void BarcodeRead(object sender, SerialDataReceivedEventArgs args)
         {
             SerialPort e = (SerialPort) sender;
             var scanned = e.ReadTo("\r");
-           AddPosition(scanned);
+            _readBarcode = scanned;
         }
 
         private void AddPosition(string code)
         {
             int amount = 1;
-            //if (_number.Text.Length != 0) amount = int.Parse(_number.Text);
-            try 
-            { 
-                _invoicePositions.Add(new CheckLine(code, amount)); 
+            if (_number.Text.Length != 0) amount = int.Parse(_number.Text);
+            try
+            {
+                var pos = new CheckLine(code, amount);
+                var asd = _invoicePositions.Where(x => x.Data.EAN13 == code);
+                if (asd.Any())
+                    asd.FirstOrDefault().Amount += amount;
+                else 
+                    _invoicePositions.Add(pos);
             }
             catch
             {
                 MessageBox.Show($"Позиция с кодом {code} не найдена, попробуте повторить операцию",
                 "Произошла ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            _updatedSomeValue = true;
+        }
+        
+        
+        private void AddPositionOnClick(object sender, RoutedEventArgs e)
+        {
+            string code = _barcodeForm.Text;
+            _readBarcode = code;
         }
         
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
@@ -63,7 +72,7 @@ namespace SemesterWork
                 userHash = BitConverter.ToString(hash.ComputeHash(Encoding.Unicode.GetBytes(password)));
             if (userInfo.Any() && userInfo[3] == userHash)
             {
-                currentUser = new User(userInfo);
+                _currentUser = new User(userInfo);
                 MainMenuActivity();
             }
             else
