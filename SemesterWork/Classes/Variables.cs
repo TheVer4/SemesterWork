aad18e452660e234698f0492c0b6bc0867685f14
BeeTwin <mr.hunterdan@mail.ru>
2020-05-13 22:33:45 +0500

    db location change

--- SemesterWork/Classes/Variables.cs
+++ SemesterWork/Classes/Variables.cs
@@ -14,7 +14,7 @@ namespace SemesterWork
         public static string BarcodeScannerPort { get; set; } = "COM4";
         public static string InstitutionName { get; set; }
         public static string InstitutionAddress { get; set; }
-        public static string DBConnectionString { get => @"Data Source = ..\..\DataBases\DataBase.db; Version=3"; private set { } }
+        public static string DBConnectionString { get => @"Data Source = .\DataBases\DataBase.db; Version=3"; private set { } }
         public static string CFGPath { get => @".\config.cfg"; private set { } }
     }
 }
